673a1a398480d29224accbf44878df43ee0eac5a
Nomesh Palakaluri <83343286+Nomesh88@users.noreply.github.com>
2024-07-19 00:25:25 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+$(window).load(function(){
+	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+
+/******************************************************************************************************************************
+Learn More Page Scroll
+*******************************************************************************************************************************/
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+/******************************************************************************************************************************
+Menu
+*******************************************************************************************************************************/ 
+(function() {
+
+	var bodyEl = document.body,
+		//content = document.querySelector( '.content-wrap' ),
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false;
+
+	function init() {
+		initEvents();
+	}
+
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleMenu );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenu );
+		}
+
+		/* close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleMenu();
+			}
+		} );
+		*/
+	}
+
+	function toggleMenu() {
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+		}
+		isOpen = !isOpen;
+	}
+
+	init();
+
+})();
+
+
+function sendEmail() {
+	e.preventDefault();
+	username=document.getElementById('name').value
+	useremail=document.getElementById('email').value
+	usermsg=document.getElementById('msg').value
+	Email.send({
+		SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+		To : 'nomeshpalakaluri@gmail.com',
+		From : useremail,
+		Subject : username,
+		Body : usermsg
+	}).then(
+	  message => alert(message)
+	);
+  }
+
+
